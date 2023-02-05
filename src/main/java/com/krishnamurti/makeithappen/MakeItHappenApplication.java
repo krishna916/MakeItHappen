fcd33f927d6cc200329412fe89b77cf845a1ce32
krishna916 <krishna916@outlook.com>
2023-02-05 13:37:31 +0530

    Initial spring/react setup

--- src/main/java/com/krishnamurti/makeithappen/MakeItHappenApplication.java
+++ src/main/java/com/krishnamurti/makeithappen/MakeItHappenApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MakeItHappenApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MakeItHappenApplication.class, args);
+		System.out.println("HELLO WORLD");
 	}
 
 }
